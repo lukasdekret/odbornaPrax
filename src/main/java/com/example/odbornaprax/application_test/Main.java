26e5823241e06399d946778d72bea71fc9cf841b
V0ltaroo <nendorans@gmail.com>
2023-08-10 09:45:58 +0200

    [feature]: Add QText and QTextArea Implementation

--- src/main/java/com/example/odbornaprax/application_test/Main.java
+++ src/main/java/com/example/odbornaprax/application_test/Main.java
@@ -2,6 +2,7 @@ package com.example.odbornaprax.application_test;
 
 import com.example.odbornaprax.framework.components.*;
 import javafx.application.Application;
+import javafx.scene.layout.VBox;
 
 public class Main extends QApplication {
 
@@ -12,10 +13,10 @@ public class Main extends QApplication {
     @Override
     public void start(QStage qstage) {
         qstage.setHeadline("HBox Experiment 1");
-        QButton qbutton = new QButton();
-        qbutton.setHeadline("test");
-        qbutton.setDimensions(30, 50);
-        qstage.setParent(qbutton);
+//        QButton qbutton = new QButton();
+//        qbutton.setHeadline("test");
+//        qbutton.setDimensions(30, 50);
+//        qstage.setParent(qbutton);
 
 //        QProgressBar qProgressBar = new QProgressBar();
 //        qProgressBar.setDimensions(50,300);
@@ -28,7 +29,20 @@ public class Main extends QApplication {
 //        qProgressIndicator.setProgressValue(0.14);
 //        System.out.println(qProgressIndicator.getProgressValue());
 //        qstage.setParent(qProgressIndicator);
-        
+
+//        VBox vBox = new VBox(); // TODO: Replace with own QVBox
+//        QText qText = new QText();
+//        qText.setContent("lalala");
+//        qText.setWrapperWidth(10.0); // 10 pixels
+//        vBox.getChildren().add(qText); // TODO: Replace with own QVBox
+//        qstage.setParent(vBox);
+
+//        QTextArea qTextArea = new QTextArea();
+//        qTextArea.setTextValue("haluska");
+//        System.out.println(qTextArea.getTextValue());
+//        qTextArea.appendText("lol");
+//        System.out.println(qTextArea.getTextValue());
+//        qstage.setParent(qTextArea);
 
     }
 }

acde645ea5280dbe2aa35a8ae41160de072dee2a
unknown <erikkulka131@gmail.com>
2023-08-11 11:36:49 +0200

    Add implementation of Hyperlink, ImageView and TextField

--- src/main/java/com/example/odbornaprax/application_test/Main.java
+++ src/main/java/com/example/odbornaprax/application_test/Main.java
@@ -1,8 +1,6 @@
 package com.example.odbornaprax.application_test;
 
-import com.example.odbornaprax.framework.components.QApplication;
-import com.example.odbornaprax.framework.components.QButton;
-import com.example.odbornaprax.framework.components.QStage;
+import com.example.odbornaprax.framework.components.*;
 import javafx.application.Application;
 
 public class Main extends QApplication {
@@ -12,11 +10,29 @@ public class Main extends QApplication {
     }
 
     @Override
-    public void start(QStage qstage) {
-        qstage.setHeadline("HBox Experiment 1");
+    public void start(QStage qstage)  {
+        /*qstage.setHeadline("HBox Experiment 1");
         QButton qbutton = new QButton();
         qbutton.setHeadline("test");
         qbutton.setDimensions(30, 50);
-        qstage.setParent(qbutton);
+        qstage.setParent(qbutton);*/
+
+        QTextField qtextfield = new QTextField();
+        qtextfield.setDimensions(30,50);
+        qtextfield.promptText("Enter Text");
+        qtextfield.getTextFromTextField();
+        qstage.setParent(qtextfield);
+
+        //TODO ImageView
+        /*QImageView qimageView = new QImageView();
+        qimageView.setNewImage("C:\\Users\\Erik\\Desktop\\Projects-Prax\\odbornaPrax-master-Kulka\\src\\main\\java\\com\\example\\odbornaprax\\pictures\\test.png");
+        qstage.setParent(qimageView);*/
+
+        /*QHyperlink qhyperlink = new QHyperlink();
+        qhyperlink.setFontStyle("-fx-underline: true; -fx-font-size:20px;-fx-font-family:'Roboto Regular';");
+        qhyperlink.setHyperlinkText("Click!");
+        qhyperlink.setAction("Clicked!");
+        qstage.setParent(qhyperlink);*/
+
     }
 }
